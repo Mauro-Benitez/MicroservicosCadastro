b86e5349e4813474a18faa4bd2c8151e8539e81f
mauro-benitez <mauro.cbenitez@gmail.com>
2025-02-18 21:09:22 -0300

    Created ProducerService

---
+++ ProducerMessager/Core/Pedido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public  Usuario Usuario { get; set; }
+
+        public  DateTime DataDeCriacao { get; set; }
+
+        public Pedido(int id, Usuario usuario, DateTime dataDeCriacao)
+        {
+            Id = id;
+            Usuario = usuario;
+            DataDeCriacao = dataDeCriacao;
+        }
+
+        public override string ToString()        
+            => $"Id do Pedido:{Id}," +
+                   $" Usuário:{Usuario.Nome}," +
+                   $" Data de Criação:{DataDeCriacao:dd/MM/yyyy}";
+        
+
+    }
+}
---
+++ ProducerMessager/Produtor/Controllers/PedidoController.cs
@@ -0,0 +1,55 @@
+﻿using Core;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace Produtor.Controllers
+{
+    [ApiController]
+    [Route("/Pedido")]
+    public class PedidoController : Controller
+    {
+
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+
+            using var connection = factory.CreateConnection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "fila",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                //publicando a mensagem
+
+                var message = JsonSerializer.Serialize(new Pedido(1,new Usuario(1,"Mauro", "mauro@email.com"),DateTime.Now));
+
+                var body =Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "fila",
+                    basicProperties: null,
+                    body: body);
+                
+            }
+            
+            
+
+            return Ok();
+        }
+    }
+}
